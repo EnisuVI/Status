2fcf04436843b96b4657f968abfc029ad90ec7a5
Charly <66905063+EnisuVI@users.noreply.github.com>
2026-04-29 14:28:31 +0200

    Going back

--- OLDER/dashboard/next.config.ts
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // On garde UNIQUEMENT l'essentiel pour les librairies SSH et Crypto
-  serverExternalPackages: ['ssh2', 'crypto-js'],
-};
-
-export default nextConfig;
